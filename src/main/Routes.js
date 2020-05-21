3004d3453b1ac27a87e06b376eec326ac8060a6c
Kamil Kowalewski <szamil128@onet.pl>
2020-05-21 23:26:11 +0200

    Add private routes

--- src/main/Routes.js
+++ src/main/Routes.js
@@ -1,23 +1,36 @@
-import React from "react";
+import React, {useContext} from "react";
 import {Route, Switch} from "react-router-dom";
 import HomePage from "../page/home-page/HomePage";
 import LoginPage from "../page/account/login-page/LoginPage";
 import RegisterPage from "../page/account/register-page/RegisterPage";
 import AccountPage from "../page/account/account-page/AccountPage";
 import {PATH_ACCOUNT, PATH_HOME, PATH_LOGIN, PATH_REGISTER} from "../constants";
+import PrivateRoute from "../component/util/private-route/PrivateRoute";
+import {AuthContext} from "../util/AuthContextProvider";
 
 export const Routes = (props) => {
 
   /*----------------------- VARIABLE REGION -----------------------*/
+  const {isUserLoggedIn} = useContext(AuthContext);
 
   /*------------------------ RETURN REGION ------------------------*/
   return (
     <Switch>
       <Route exact path={PATH_HOME} component={HomePage}/>
 
-      <Route exact path={PATH_LOGIN} component={LoginPage}/>
-      <Route exact path={PATH_REGISTER} component={RegisterPage}/>
-      <Route exact path={PATH_ACCOUNT} component={AccountPage}/>
+      <PrivateRoute
+        exact path={PATH_LOGIN} component={LoginPage}
+        redirectPath={PATH_ACCOUNT} privacyCondition={!isUserLoggedIn}
+      />
+      <PrivateRoute
+        exact path={PATH_REGISTER} component={RegisterPage}
+        redirectPath={PATH_LOGIN} privacyCondition={!isUserLoggedIn}
+      />
+      <PrivateRoute
+        exact path={PATH_ACCOUNT} component={AccountPage}
+        redirectPath={PATH_LOGIN} privacyCondition={isUserLoggedIn}
+      />
+
     </Switch>
   );
 };
