768255224e178f75dc288c24faa4abd9d52df5fa
Kamil Kowalewski <szamil24@gmail.com>
2020-06-30 22:34:38 +0200

    Optimize imports

--- src/page/account/account-page/AccountPage.js
+++ src/page/account/account-page/AccountPage.js
@@ -1,15 +1,20 @@
 import React from "react";
-import propTypes from "prop-types";
+import GlobalStyles from "../../../main/GlobalStyles";
+import "../../../index.css";
+import Avatar from "@material-ui/core/Avatar";
 
 export const AccountPage = (props) => {
 
   /*----------------------- VARIABLE REGION -----------------------*/
-
+  const globalStyles = GlobalStyles();
+  console.log()
   /*------------------------ RETURN REGION ------------------------*/
   return (
-    <>
-
-    </>
+    <div className="container-fluid custom-container-xl  text-white mt-5">
+      <div className={globalStyles.materialBlueBackground}>
+<Avatar />
+      </div>
+    </div>
   );
 };
 
