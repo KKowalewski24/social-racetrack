c5df00e0e91877eded42f5a0b356eb1e3c0a9ba5
Kamil Kowalewski <szamil24@gmail.com>
2020-07-01 20:55:19 +0200

    Finish DisplayUserData.js

--- src/component/display-user-data/DisplayUserData.js
+++ src/component/display-user-data/DisplayUserData.js
@@ -15,7 +15,7 @@ export const DisplayUserData = (props) => {
     );
   };
 
-  const renderHeader = (text) => {
+  const renderHeaderTitle = (text) => {
     return (
       <div className="row justify-content-center mb-3">
         <div className="col-md-12 text-center font-weight-bold custom-font-size-1-5">
@@ -26,73 +26,98 @@ export const DisplayUserData = (props) => {
   };
 
   const renderUserInformation = () => {
+
+    const renderLeftSide = () => {
+      return (
+        <div className="col-md-6 text-center">
+          {renderBoldText(strings.accountPage.country + ": " + props.country)}
+          {renderBoldText(strings.accountPage.city + ": " + props.city)}
+          {
+            props.joinDate ?
+              renderBoldText(strings.accountPage.joinDate + ": " + props.joinDate)
+              : null
+          }
+        </div>
+      );
+    };
+
+    const renderRightSide = () => {
+      return (
+        <div className="col-md-6 text-center">
+          {renderBoldText(strings.accountPage.age + ": " + props.age)}
+          {
+            props.lastLogin ?
+              renderBoldText(strings.accountPage.lastLogin + ": " + props.lastLogin)
+              : null
+          }
+        </div>
+      );
+    };
+
     return (
       <HorizontalContainer panelBackgroundColor={props.panelBackgroundColor}>
-        {renderHeader(strings.accountPage.name + ": " + props.firstName + " " + props.lastName)}
+        {renderHeaderTitle(strings.accountPage.name + ": " + props.firstName + " " + props.lastName)}
 
         <div className="row justify-content-center custom-font-size-1">
-          <div className="col-md-6 text-center">
-            {renderBoldText(strings.accountPage.country + ": " + props.country)}
-            {renderBoldText(strings.accountPage.city + ": " + props.city)}
-            {
-              props.joinDate ?
-                renderBoldText(strings.accountPage.joinDate + ": " + props.joinDate)
-                : null
-            }
-          </div>
-
-          <div className="col-md-6 text-center">
-            {renderBoldText(strings.accountPage.age + ": " + props.age)}
-            {
-              props.lastLogin ?
-                renderBoldText(strings.accountPage.lastLogin + ": " + props.lastLogin)
-                : null
-            }
-          </div>
+          {renderLeftSide()}
+          {renderRightSide()}
         </div>
       </HorizontalContainer>
     );
   };
 
   const renderUserCars = () => {
+
+    const renderHead = () => {
+      return (
+        <thead>
+          <tr>
+            <th>{strings.accountPage.brand}</th>
+            <th>{strings.accountPage.model}</th>
+            <th>{strings.accountPage.productionYear}</th>
+            <th>{strings.accountPage.carType}</th>
+            <th>{strings.accountPage.mileage}</th>
+            <th>{strings.accountPage.engineType}</th>
+            <th>{strings.accountPage.enginePower}</th>
+            <th>{strings.accountPage.driveTrainType}</th>
+          </tr>
+        </thead>
+      );
+    };
+
+    const renderBody = () => {
+      return (
+        <tbody>
+          {
+            props.carsArray.map((it, index) => {
+              return (
+                <tr key={index}>
+                  <td>{it.brand}</td>
+                  <td>{it.model}</td>
+                  <td>{it.productionYear}</td>
+                  <td>{it.carType}</td>
+                  <td>{it.mileage}</td>
+                  <td>{it.engineType}</td>
+                  <td>{it.enginePower}</td>
+                  <td>{it.driveTrainType}</td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      );
+    };
+
     return (
       <HorizontalContainer panelBackgroundColor={props.panelBackgroundColor}>
-        {renderHeader(strings.accountPage.cars)}
+        {renderHeaderTitle(strings.accountPage.cars)}
 
         <div className="row justify-content-center">
           <table
             className="table table-responsive table-striped w-auto custom-font-size-0-5 text-white"
           >
-            <thead>
-              <tr>
-                <th>{strings.accountPage.brand}</th>
-                <th>{strings.accountPage.model}</th>
-                <th>{strings.accountPage.productionYear}</th>
-                <th>{strings.accountPage.carType}</th>
-                <th>{strings.accountPage.mileage}</th>
-                <th>{strings.accountPage.engineType}</th>
-                <th>{strings.accountPage.enginePower}</th>
-                <th>{strings.accountPage.driveTrainType}</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                props.carsArray.map((it, index) => {
-                  return (
-                    <tr key={index}>
-                      <td>{it.brand}</td>
-                      <td>{it.model}</td>
-                      <td>{it.productionYear}</td>
-                      <td>{it.carType}</td>
-                      <td>{it.mileage}</td>
-                      <td>{it.engineType}</td>
-                      <td>{it.enginePower}</td>
-                      <td>{it.driveTrainType}</td>
-                    </tr>
-                  );
-                })
-              }
-            </tbody>
+            {renderHead()}
+            {renderBody()}
           </table>
         </div>
       </HorizontalContainer>
@@ -100,32 +125,45 @@ export const DisplayUserData = (props) => {
   };
 
   const renderUserAwards = () => {
+
+    const renderHead = () => {
+      return (
+        <thead>
+          <tr>
+            <th>{strings.accountPage.description}</th>
+            <th>{strings.accountPage.year}</th>
+          </tr>
+        </thead>
+      );
+    };
+
+    const renderBody = () => {
+      return (
+        <tbody>
+          {
+            props.receivedAwardsArray.map((it, index) => {
+              return (
+                <tr key={index}>
+                  <td>{it.description}</td>
+                  <td>{it.year}</td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      );
+    };
+
     return (
       <HorizontalContainer panelBackgroundColor={props.panelBackgroundColor}>
-        {renderHeader(strings.accountPage.awards)}
+        {renderHeaderTitle(strings.accountPage.awards)}
 
         <div className="row justify-content-center">
           <table
             className="table table-responsive table-striped w-auto custom-font-size-0-5 text-white"
           >
-            <thead>
-              <tr>
-                <th>{strings.accountPage.description}</th>
-                <th>{strings.accountPage.year}</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                props.receivedAwardsArray.map((it, index) => {
-                  return (
-                    <tr key={index}>
-                      <td>{it.description}</td>
-                      <td>{it.year}</td>
-                    </tr>
-                  );
-                })
-              }
-            </tbody>
+            {renderHead()}
+            {renderBody()}
           </table>
         </div>
       </HorizontalContainer>
