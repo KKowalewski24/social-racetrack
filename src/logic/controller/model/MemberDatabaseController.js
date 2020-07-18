115de6bce239a4225045c05a22eb929c3b2cfdc8
Kamil Kowalewski <szamil24@gmail.com>
2020-07-18 19:36:24 +0200

    Add impl of MemberDatabaseController.js + improve AccountController.js

--- src/logic/controller/model/MemberDatabaseController.js
+++ src/logic/controller/model/MemberDatabaseController.js
@@ -1,4 +1,6 @@
 import {DatabaseController} from "../DatabaseController";
+import {PATH_DB_COLLECTION_MEMBERS} from "../../../config/constant/firebase-constants";
+import {convertClassObjectToJsObject, PR} from "../../Helper";
 
 export class MemberDatabaseController {
 
@@ -6,19 +8,46 @@ export class MemberDatabaseController {
   _databaseController = new DatabaseController();
 
   /*------------------------ METHODS REGION ------------------------*/
-  createMember = () => {
+  createMember = async (data = PR(), errorFunction = PR()) => {
+    try {
+      return await this._databaseController.createData(
+        PATH_DB_COLLECTION_MEMBERS + data.id,
+        convertClassObjectToJsObject(data),
+        errorFunction
+      );
+    } catch (err) {
+      errorFunction();
+    }
+  };
+
+  readMemberById = async (id = PR(), errorFunction = PR()) => {
+    try {
+      return await this._databaseController
+        .readSingleData(PATH_DB_COLLECTION_MEMBERS + id, errorFunction);
+    } catch (err) {
+      errorFunction();
+    }
+  };
+
+  readAllMembers = async (errorFunction = PR()) => {
+    try {
+      return await this._databaseController
+        .readAllData(PATH_DB_COLLECTION_MEMBERS, errorFunction);
+    } catch (err) {
+      errorFunction();
+    }
+  };
+
+  updateMember = async () => {
 //TODO
   };
 
-  readMember = () => {
-//TODO
-  };
-
-  updateMember = () => {
-//TODO
-  };
-
-  deleteMember = () => {
-//TODO
+  deleteMember = async (id = PR(), errorFunction = PR()) => {
+    try {
+      return await this._databaseController
+        .deleteData(PATH_DB_COLLECTION_MEMBERS + id, errorFunction);
+    } catch (err) {
+      errorFunction();
+    }
   };
 }
