c5d7bd4a4a2bf818d992a225714221f3c512d98b
Kamil Kowalewski <szamil24@gmail.com>
2020-07-23 21:58:31 +0200

    Add reading single event by ID

--- src/logic/controller/model/EventDatabaseController.js
+++ src/logic/controller/model/EventDatabaseController.js
@@ -27,8 +27,34 @@ export class EventDatabaseController {
     }
   };
 
-  readSingleEventByID = async () => {
-//TODO
+  /**
+   * Here has been done weird trick, after fetching document from DB,
+   * refPath are resolved to get `real` data not to refPath, in order to do this to event object
+   * new properties are added and to have access to `real` use this created properties
+   */
+  readSingleEventByID = async (id = PR(), errorFunction = PR()) => {
+    try {
+      const event = await this._databaseController
+        .readSingleData(PATH_DB_COLLECTION_EVENTS + id, errorFunction);
+
+      event.racetrackData = await this._databaseController
+        .readSingleData(event.racetrackRefPath, errorFunction);
+
+      event.eventCreatorData = await this._databaseController
+        .readSingleData(event.eventCreatorRefPath, errorFunction);
+
+      event.membersDataArray = [];
+      for (const it of event.membersRefPathArray) {
+        event.membersDataArray.push(
+          await this._databaseController.readSingleData(it, errorFunction)
+        );
+      }
+
+      return event;
+
+    } catch (err) {
+      errorFunction();
+    }
   };
 
   readAllEvents = async () => {
